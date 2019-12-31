f78a52a2fb6a1dfac46f3cff902b8c0c120c68d8
SoloBlackBox <solomonwagner@protonmail.com>
2019-12-30 20:02:06 -0500

    Switching to a file tree based data system for master
    Signed-off-by: SoloBlackBox <SolomonWagner@protonmail.com>

--- src/main/java/bot/discord/letitrip/App.java
+++ src/main/java/bot/discord/letitrip/App.java
@@ -1,10 +1,12 @@
 package bot.discord.letitrip;
 
+import org.javacord.api.event.message.MessageCreateEvent;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-class App {
+public class App {
     static String loginToken() {
         String token = null;
         try {
@@ -15,5 +17,15 @@ class App {
             e.printStackTrace();
         }
         return token;
+    }
+    public static void notifyServerOwner(MessageCreateEvent event, String message) {
+        event.getChannel().sendMessage(event.getServer().get().getOwner().getMentionTag() + " " + message);
+    }
+    public static void sendError(MessageCreateEvent event, String message) {
+        event.getChannel().sendMessage(message);
+    }
+    public static void warnAll(MessageCreateEvent event, String message) {
+        notifyServerOwner(event,message);
+        sendError(event,message);
     }
 }
