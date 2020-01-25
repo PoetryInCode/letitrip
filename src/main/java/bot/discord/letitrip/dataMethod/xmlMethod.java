e8912215f0b976c1b050e8a9e3bb7d195a934245
SoloBlackBox <solomonwagner@protonmail.com>
2020-01-25 09:27:59 -0500

    maybe switching back to xml method
    Signed-off-by: Solo Blackbox <SolomonWagner@protonmail.com>

--- src/main/java/bot/discord/letitrip/dataMethod/xmlMethod.java
+++ src/main/java/bot/discord/letitrip/dataMethod/xmlMethod.java
@@ -7,44 +7,51 @@ import org.w3c.dom.Element;
 
 import bot.discord.letitrip.App;
 
+import java.io.File;
+
 public class xmlMethod {
-    public static Element addServer(Document doc, MessageCreateEvent event) {
-        Element server = doc.createElement("server");
-        Element sIdElem = doc.createElement("serverId");
-        Element users = doc.createElement("users");
-        doc.getChildNodes().item(0).appendChild(server);
+    static MessageCreateEvent Event;
+    static Document Doc;
+    public static void initXmlMethod(File file, MessageCreateEvent event) {
+        Event=event;
+    }
+    public static Element addServer() {
+        Element server = Doc.createElement("server");
+        Element sIdElem = Doc.createElement("serverId");
+        Element users = Doc.createElement("users");
+        Doc.getChildNodes().item(0).appendChild(server);
         server.appendChild(sIdElem);
         users.appendChild(server);
         return server;
     }
 
-    public static boolean isInitialized(String id, Document document) {
-        return document.getElementById(id) != null;
+    public static boolean isInitialized(String id) {
+        return Doc.getElementById(id) != null;
     }
-    public static boolean serverInitialized(MessageCreateEvent event, Document document) {
+    public static boolean serverInitialized() {
         boolean initialized = false;
-        if(event.getServer().isPresent()) {
-            if (isInitialized(event.getServer().get().getIdAsString(), document)) {
+        if(Event.getServer().isPresent()) {
+            if (isInitialized(Event.getServer().get().getIdAsString())) {
                 initialized = true;
             } else {
-                App.notifyServerOwner(event, "Your server is not set up with the letitrip bot. Would you like to set it up now?" +
+                App.notifyServerOwner(Event, "Your server is not set up with the letitrip bot. Would you like to set it up now?" +
                         " Run --setup to enable the bot");
             }
         }
         return initialized;
     }
-    public static boolean userInitialized(User user, Document document) {
-        return isInitialized(user.getIdAsString(), document);
+    public static boolean userInitialized(User user) {
+        return isInitialized(user.getIdAsString());
     }
 
-    public static Element addUser(MessageCreateEvent event, User user, Document document) {
-        Element server = document.getElementById(event.getServer().get().getIdAsString());
+    public static Element addUser(User user) {
+        Element server = Doc.getElementById(Event.getServer().get().getIdAsString());
         Element newUser = null;
-        if(serverInitialized(event, document)) {
-            if(userInitialized(user, document)) {
+        if(serverInitialized()) {
+            if(userInitialized(user)) {
                 System.err.println("User already initialized");
             } else {
-                newUser = document.createElement("user");
+                newUser = Doc.createElement("user");
                 newUser.setIdAttribute(user.getIdAsString(),true);
                 Element users = (Element) server.getChildNodes().item(0);
                 users.appendChild(newUser);
