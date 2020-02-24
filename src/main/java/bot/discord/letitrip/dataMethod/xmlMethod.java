5ac43685975efae81d4eb734b18ec26e9722c8ec
Solo Blackbox <SolomonWagner@protonmail.com>
2020-02-24 09:46:16 -0500

    probably switching to sax parser for efficiency

--- src/main/java/bot/discord/letitrip/dataMethod/xmlMethod.java
+++ src/main/java/bot/discord/letitrip/dataMethod/xmlMethod.java
@@ -6,15 +6,57 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import bot.discord.letitrip.App;
+import org.xml.sax.Attributes;
+import org.xml.sax.Parser;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
+import javax.xml.parsers.*;
 import java.io.File;
+import java.io.IOException;
 
 public class xmlMethod {
+
+    SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+    SAXParser saxParser;
+
     static MessageCreateEvent Event;
     static Document Doc;
+    static File _file;
+
+
+    {
+        try {
+            saxParser = saxParserFactory.newSAXParser();
+            class handler extends DefaultHandler {
+                @Override
+                public void startDocument() throws SAXException {
+                    super.startDocument();
+                }
+
+                @Override
+                public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                    super.startElement(uri, localName, qName, attributes);
+                    if(localName.equals("")) {}
+                }
+            }
+            saxParser.parse(_file, new handler());
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void initXmlMethod(File file, MessageCreateEvent event) {
         Event=event;
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = builderFactory.newDocumentBuilder();
+            Doc = builder.parse(file);
+        } catch (IOException | ParserConfigurationException | SAXException e) {
+            App.sysLog(e.getMessage());
+        }
     }
+
     public static Element addServer() {
         Element server = Doc.createElement("server");
         Element sIdElem = Doc.createElement("serverId");
